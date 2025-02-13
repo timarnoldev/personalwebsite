c6ef5382caa15880cbd056b4ac906aa3b569964f
Tim Arnold <hello@tim-arnold.de>
2025-02-13 20:48:02 +0100

    Added favicon and fulda hackathon

--- src/components/awards.tsx
+++ src/components/awards.tsx
@@ -34,7 +34,16 @@ const awards = (lang:Dictionary) => {
             link: "https://www.linkedin.com/posts/timarnold-_ein-spannender-tag-in-berlin-ging-gestern-activity-7266103194676613121-Iv_W"
 
         },
-       
+        {
+            name: lang.awardsFuldaHackathonHeadline,
+            date: "10/2024",
+            image: "/awards/hackathon-fulda.svg",
+            localation: "Fulda, Germany",
+            text: lang.awardsFuldaHackathonText,
+            reference: "external",
+            link: "https://www.linkedin.com/posts/timarnold-_gestern-ging-der-6-fulda-hackathon-in-der-activity-7256757551994736642-Fo2T"
+
+        },
     ]
 }
 
@@ -46,19 +55,19 @@ export default function Awards(props: { lang: Dictionary }) {
         </div>
 
         {/*Has page width*/}
-            <div className="flex md:flex-row flex-col  max-w-400 mt-4 mb-5 xl:gap-16 gap-8 mx-10"> {/*Has width of all children*/}
+            <div className="flex flex-col xl:flex-row xl:flex md:grid md:grid-cols-2  max-w-400 mt-4 mb-5 xl:gap-8 gap-8 mx-10"> {/*Has width of all children*/}
 
                 {
                     awards(props.lang).map((award, index) => {
                         return <div key={index} className="flex flex-1 flex-col justify-between gap-4 max-w-[80vw]">
                             <div className="flex flex-col gap-3 sm:items-center">
                                 {
-                                    award.image && <div className="lg:m-10 m-5 bg-white shadow-2xl p-4 rounded-xl aspect-square flex flex-col items-center justify-center">
+                                    award.image && <div className="lg:m-10 m-5 max-w-100 bg-white shadow-2xl p-4 rounded-xl aspect-square flex flex-col items-center justify-center">
                                         <Image src={award.image} alt="Logo" width={600} height={600} className="  "/>
                                     </div>
                                 }
                                 
-                                <h2 className="text-xl font-bold flex flex-row gap-2 mt-3 items-center">{award.name}</h2>
+                                <h2 className="text-xl font-bold flex flex-row gap-2 mt-3 items-center text-center">{award.name}</h2>
                                 <div className="flex flex-row gap-4">
                                     <div className="text-gray-600 flex flex-row gap-2 items-center"><CalendarDays />{award.date}</div>
                                     <div className="text-gray-600 flex flex-row gap-2 items-center"><MapPin />{award.localation}</div>
