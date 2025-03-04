731509c17434286720133b11b07c50a736827634
Tim Arnold <hello@tim-arnold.de>
2025-03-04 11:14:37 +0100

    Updated robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://tim-arnold.de',
-    generateRobotsTxt: false, // (optional)
+    generateRobotsTxt: true, // (optional)
     // ...other options
   }
