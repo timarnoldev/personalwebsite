9a46ba4cfa93386007540e0de33ce8b7885e2c32
Tim Arnold <hello@tim-arnold.de>
2025-03-04 11:07:07 +0100

    fixed typo

--- src/middleware.ts
+++ src/middleware.ts
@@ -36,8 +36,8 @@ export function middleware(request: NextRequest) {
        '/mainimage.webp',
        '/timarnold.webp',
        '/wave.webp',
-       'sitemap.xml',
-       'sitemap-0.xml'
+       '/sitemap.xml',
+       '/sitemap-0.xml'
      ].includes(pathname.toLowerCase())
    )
      return
