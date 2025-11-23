51525ef3756fea79c357932b038c57eb6bf7360c
Tim Arnold <hello@tim-arnold.de>
2025-11-23 12:59:30 +0100

    Removed unused component

--- src/components/articles/BlogEntry.tsx
+++ src/components/articles/BlogEntry.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { LanguageContext } from "@/context/LanguageContext";
-import { ArrowUpRight, ExternalLink, MoveUpRight } from "lucide-react"
+import { ArrowUpRight, ExternalLink } from "lucide-react"
 import Image from "next/image"
 import { useContext } from "react";
 
